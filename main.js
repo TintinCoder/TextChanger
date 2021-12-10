b66a96de3793671b9d39c6c78bd73fa7dfb1dcde
TintinCoder <81453801+TintinCoder@users.noreply.github.com>
2021-12-10 14:45:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Working')
+var video;
+var canvas;
+var poseNet;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(800, 170)
+
+    poseNet = ml5.poseNet(video, modelReady);
+    poseNet.on('pose', gotPoses)
+}
+function draw(){
+    background('#636d78')
+}
+
+function modelReady(){
+    console.log('poseNet is ready');
+}
+
+function gotPoses(results){
+    if(results.length > 0) {
+        console.log(results)
+    }
+}
